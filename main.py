bad3c5eb0ed73bc10b5bd25e8a5be58bc645fb8d
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-11-14 15:47:44 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from robyn import Robyn
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PORT = os.environ.get("PORT")
+
+app = Robyn(__file__)
+
+@app.get("/")
+async def h(request):
+    return "Hello, world!"
+
+app.start(port=PORT)
